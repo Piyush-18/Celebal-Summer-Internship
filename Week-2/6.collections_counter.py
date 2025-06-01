b703f10239eef4e3b718c8b80b24398e119e2347
Piyush Pankaj Panigrahi <95301249+Piyush-18@users.noreply.github.com>
2025-06-01 19:03:34 +0530

    Update 6.collections_counter.py

--- Week-2/6.collections_counter.py
+++ Week-2/6.collections_counter.py
@@ -12,15 +12,15 @@ inventory = Counter(shoe_sizes)
 # Input: number of customers
 N = int(input())
 
-# Variable to store total earnings
+
 earnings = 0
 
-# Process each customer
+
 for _ in range(N):
     size, price = map(int, input().split())
     if inventory[size] > 0:
         earnings += price
         inventory[size] -= 1  # Sell the shoe (reduce count)
 
-# Output the total earnings
+
 print(earnings)
